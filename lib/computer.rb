2fbb627e9c5e7ec942de44f0e2f6595a893a4da8
Jorge Santos <jorgeoliveirasantos@gmail.com>
2022-08-28 14:28:32 +0900

    Initial project boilerplate
    - Specify Ruby version
    - Add lib/ and test/ folders
    - Add target program to run on the simulator
    - Minitest boilerplate (unit test and Rakefile)

---
+++ lib/computer.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# Computer Simulator
+class Computer; end
---
+++ test/computer_test.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/computer'
+
+describe Computer do; end
